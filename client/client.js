09fa7f99b5aa6298f08e8f79b54e7dc3e891ab94
Ian McCunn <ianmccunn@gmail.com>
2015-06-14 20:42:28 -0700

    more to client

--- client/client.js
+++ client/client.js
@@ -1,13 +1,37 @@
 'use strict';
 
 var net = require('net');
-var args = process.argv;
+var args = process.argv.slice(2);
 
 var con = {
-  port: args[2] || 3000,
-  host: args[3] || '127.0.0.1'
+  port: args[0] || 3000,
+  host: args[1] || '127.0.0.1'
 };
+
+var clientOptions = {
+	host: 'localhost',
+	port: con.port,
+ 	path: '/',
+	method: 'GET'
+};	
 
 var connection = net.createConnection(con, function() {
   console.log('Connection successful');
+});
+
+
+
+var clientReq = http.request(clientOptions, function(res) {
+	console.log('status code: ', res.statusCode);
+	switch(res.statusCode) {
+		case 200: 
+						res.setEncoding('utf8');
+						res.on('data', function(data) {
+							console.log('data: ', data);
+						});
+						break;
+		case 404:
+						console.log('404 error');
+						break;
+	}
 });
